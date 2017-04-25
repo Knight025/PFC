e39f2f25815a78daf7d25208eb2ad4c461b33cb1
Knight025 <srealvaldes@gmail.com>
2017-04-25 12:24:00 +0200

    Update serial.c

--- src/serial.c
+++ src/serial.c
@@ -27,15 +27,15 @@ struct SCI_PKG *getPkg(){
 // Test 1,SCIA  DLB, 8-bit word, baud rate 0x000F, default, 1 STOP bit, no parity
 void scia_init(uint32_t baudRate){
 	uint32_t BRR = 50000000/(baudRate*8 -1);
-    // Note: Clocks were turned on to the SCIA peripheral
-    // in the InitSysCtrl() function
+	// Note: Clocks were turned on to the SCIA peripheral
+	// in the InitSysCtrl() function
 
 	// (Santi) las siguientes 3 lineas de codigo constituian una funcion aparte,
 	// scia_fifo_init()
 	// Initialize the SCI FIFO:
-    SciaRegs.SCIFFTX.all = 0xE040;
-    SciaRegs.SCIFFRX.all = 0x2044;
-    SciaRegs.SCIFFCT.all = 0x0;
+	SciaRegs.SCIFFTX.all = 0xE040;
+	SciaRegs.SCIFFRX.all = 0x2044;
+	SciaRegs.SCIFFCT.all = 0x0;
 
 
 
@@ -48,15 +48,13 @@ void scia_init(uint32_t baudRate){
 	SciaRegs.SCICTL2.bit.TXINTENA = 1;
 	SciaRegs.SCICTL2.bit.RXBKINTENA = 1;
 
-    //
-    // SCIA at 9600 baud
-    // @LSPCLK = 50 MHz (200 MHz SYSCLK) HBAUD = 0x02 and LBAUD = 0x8B.
-    // @LSPCLK = 30 MHz (120 MHz SYSCLK) HBAUD = 0x01 and LBAUD = 0x86.
-    //
+	//
+	// SCIA at 9600 baud
+	// @LSPCLK = 50 MHz (200 MHz SYSCLK) HBAUD = 0x02 and LBAUD = 0x8B.
+	// @LSPCLK = 30 MHz (120 MHz SYSCLK) HBAUD = 0x01 and LBAUD = 0x86.
 
 	//SciaRegs.SCIHBAUD.all = 0x0002;	Esto es para 9600 Baud
-    //SciaRegs.SCILBAUD.all = 0x008B;
-
+	//SciaRegs.SCILBAUD.all = 0x008B;
 
 	SciaRegs.SCIHBAUD.all = (0xFF00 & BRR)>>8;
 	SciaRegs.SCILBAUD.all = 0x00FF & BRR;
@@ -66,29 +64,29 @@ void scia_init(uint32_t baudRate){
 
 // Transmit a character from the SCI
 void scia_xmit(uint16_t a){
-    while (SciaRegs.SCIFFTX.bit.TXFFST != 0) {}
-    SciaRegs.SCITXBUF.bit.TXDT = ((0xFF00)&a)>>8;
-    while (SciaRegs.SCIFFTX.bit.TXFFST != 0) {}
-    SciaRegs.SCITXBUF.bit.TXDT = 0x00FF&a;
+	while (SciaRegs.SCIFFTX.bit.TXFFST != 0) {}
+	SciaRegs.SCITXBUF.bit.TXDT = ((0xFF00)&a)>>8;
+	while (SciaRegs.SCIFFTX.bit.TXFFST != 0) {}
+	SciaRegs.SCITXBUF.bit.TXDT = 0x00FF&a;
 }
 
 void scia_pkg(uint16_t * msg, uint16_t length){
-    int i;
-    scia_xmit(2*length+2);
-    for(i = 0; i < length; i++){
-        scia_xmit(msg[i]);
-    }
+	int i;
+	scia_xmit(2*length+2);
+	for(i = 0; i < length; i++){
+		scia_xmit(msg[i]);
+	}
 }
 
 void scia_msg(char* msg, int partitioned){
-    int i = 0;
-    if(partitioned == 0)
-        scia_xmit(strlen(msg)+2);
+	int i = 0;
+	if(partitioned == 0)
+		scia_xmit(strlen(msg)+2);
 
-    while(msg[i] != '\0'){
-    	while (SciaRegs.SCIFFTX.bit.TXFFST != 0) {}
-    	SciaRegs.SCITXBUF.bit.TXDT = 0x00FF & msg[i++];
-    }
+	while(msg[i] != '\0'){
+		while (SciaRegs.SCIFFTX.bit.TXFFST != 0) {}
+		SciaRegs.SCITXBUF.bit.TXDT = 0x00FF & msg[i++];
+	}
 
 }
 
